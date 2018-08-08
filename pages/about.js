af7246b402ead1687c06692b35202962128d315c
alyoshas <evelynschmitz1@gmail.com>
2018-08-08 20:17:58 -0300

    Add pages: contact and about.

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../layouts/Main'
+
+export default () => (
+    <Layout>
+       <p>This is the about page</p>
+    </Layout>
+)
---
+++ pages/contact.js
@@ -0,0 +1,6 @@
+import Layout from '../layouts/Main'
+export default () => (
+    <Layout>
+       <p>This is the contact page</p>
+    </Layout>
+)
